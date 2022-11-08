069a2062af8f94c252c1bd277baf2aa7117d384c
Ju Leiite <105261049+JLeiite@users.noreply.github.com>
2022-11-08 18:26:06 -0300

    Checkpoint 1 - Front end 2
    Arquivo cumprindo todos os requisitos solicitados no trabalho.

---
+++ script.js
@@ -0,0 +1,66 @@
+// Recebendo valores do formulario
+let inputDestino = document.querySelector("#destino");
+let inputDescricao = document.querySelector("#descricao");
+let inputImagem = document.querySelector("#imagem");
+let inputNota = document.querySelector("#nota");
+let inputColor = document.querySelector("#color");
+
+// BOTÃO > Definindo validação do botão
+const formControl = document.querySelector("form");
+const formButton = document.querySelector(".form-button");
+
+// BOTÃO > Definindo botão desabilitado como padrão
+formButton.disabled = true;
+formControl.addEventListener("keyup", validarBotao);
+function validarBotao() {
+    if (inputDestino.value === "" || inputDescricao.value === "" || inputImagem.value === "" || inputNota.value === "") {
+        formButton.disabled = true; // Desabilitar botão enquanto input estiver vazio
+    }else if(inputDestino.value.length < 4){
+        formButton.disabled = true; // Desabilitar botão enquanto o valor da descrição for menor que 4 caracteres
+        alert("Insira um destino com mais de 4 letras");
+    }else if(inputDescricao.value.length < 4){
+        formButton.disabled = true; // Desabilitar botão enquanto o valor da descrição for menor que 4 caracteres
+        alert("Insira uma descrição maior");
+    }
+    else{
+        formButton.disabled = false; // Habilitar botão enquanto input tiver preenchido
+    }
+}
+// BOTÃO > Limpando dados do formulário
+function clearData() {
+    inputDestino.value = ''; 
+    inputDescricao.value = '';
+    inputImagem.value = '';
+    inputNota.value = '';
+    formButton.disabled = true;
+}
+
+// CARD > Adicionar card
+let formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Criando elementos
+    let addCard = document.createElement("a");
+
+    // Define o que vai ser adicionado na página
+    addCard.innerHTML = `
+        <div class="card bg-light border-secondary p-2 mt-3 ml-3" style="width: 20rem;">
+        <img class="card-img-top" src="${inputImagem.value}" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title"> ${inputDestino.value} <span class="badge text-light small" style="background-color:${inputColor.value}"><i class="bi bi-star"></i> ${inputNota.value}</span> </h5>
+        <p class="card-text">${inputDescricao.value}</p>
+        </div>
+        </div>`;
+        
+    // Resetanto inputs
+    clearData();
+        
+    // Definindo onde vai ser adicionado
+    const card = document.querySelector(".addCard");
+
+    // Adicionando ao código
+    card.appendChild(addCard);
+       
+})
