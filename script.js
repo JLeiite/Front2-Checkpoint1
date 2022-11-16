97fb92f9e5e83679185f011f67d732fffc03bdd6
Ju Leiite <105261049+JLeiite@users.noreply.github.com>
2022-11-16 20:44:31 -0300

    Adições finais
    Inserindo botão de close e melhorando validação.

--- script.js
+++ script.js
@@ -9,19 +9,35 @@ let inputColor = document.querySelector("#color");
 const formControl = document.querySelector("form");
 const formButton = document.querySelector(".form-button");
 
+// FORM > Validação
+function validarInput() {
+
+    // Validando o Destino
+    if(inputDestino.value.length < 4){
+        inputDestino.classList.add("is-invalid");
+    }
+    else {inputDestino.classList.remove("is-invalid")};
+        
+    // Validando a Descrição
+    if(inputDescricao.value.length < 4){
+          inputDescricao.classList.add("is-invalid");
+    }
+    else{inputDescricao.classList.remove("is-invalid")};
+}
+
 // BOTÃO > Definindo botão desabilitado como padrão
 formButton.disabled = true;
-
 formControl.addEventListener("keyup", validarBotao);
 function validarBotao() {
     if (inputDestino.value === "" || inputDescricao.value === "" || inputImagem.value === "" || inputNota.value === "") {
         formButton.disabled = true; // Desabilitar botão enquanto input estiver vazio
-    }else if(inputDestino.value.length < 4){
-        formButton.disabled = true; // Desabilitar botão enquanto o valor da descrição for menor que 4 caracteres
-        alert("Destino precisa ter mais de 4 itens");
-    }else if(inputDescricao.value.length < 4){
-        formButton.disabled = true; // Desabilitar botão enquanto o valor da descrição for menor que 4 caracteres
-        alert("Insira uma descrição maior");
+    }
+    else if (inputDestino.value.length < 4 || inputDescricao.value.length < 4){
+        
+        formButton.disabled = true; // Desabilita enquanto não atender a condição
+        
+        // Validando os inputs
+        formControl.addEventListener("keyup", validarInput);
     }
     else{
         formButton.disabled = false; // Habilitar botão enquanto input tiver preenchido
@@ -48,7 +64,8 @@ formulario.addEventListener("submit", function(event) {
     // Define o que vai ser adicionado na página
     addCard.innerHTML = `
         <div class="card bg-light border-secondary p-2 mt-3 ml-3" style="width: 20rem;">
-        <img class="card-img-top" src="${inputImagem.value}" alt="Card image cap">
+        <div id="close" onclick="eliminaCard()" class="bg-dark" style="position: absolute; padding: 2px 8px; z-index: 2; opacity: 0.7; right: 8px; cursor: pointer"><i class="bi bi-x-circle-fill text-white" style="font-size: 14pt"></i></div>
+        <img class="card-img-top" src="${inputImagem.value}" alt="Imagem cidade">
         <div class="card-body">
         <h5 class="card-title"> ${destino.value} <span class="badge text-light small" style="background-color:${color.value}"><i class="bi bi-star"></i> ${nota.value}</span> </h5>
         <p class="card-text">${descricao.value}</p>
@@ -62,5 +79,13 @@ formulario.addEventListener("submit", function(event) {
     const card = document.querySelector(".addCard");
 
     // Adicionando ao código
-    card.appendChild(addCard);   
+    // Adiciona o card no final > card.appendChild(addCard);  
+    // Adiciona o card no início
+    card.prepend(addCard);  
 })
+
+// Eliminando o card
+function eliminaCard() {
+    let esteCard = document.querySelector(".card");
+    esteCard.parentNode.removeChild(esteCard);
+  }
